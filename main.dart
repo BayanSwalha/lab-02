bf3ee58f2e9a5343b0bed032969ed3500c52cdfb
BayanSwalha <bayanswalha114@gmail.com>
2022-08-28 12:21:23 +0300

    upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  List myColors = ["blue", "purple", "green", "orange", "black", "amber"];
+
+  for (int i = 0; i < myColors.length; i++) {
+    String color = myColors[i];
+    if ('${color[0]}' == 'b' || '${color[0]}' == 'a') {
+      print(myColors[i].toString().toUpperCase());
+    } else {
+      int c = color.length;
+      print(
+          "sorry this $color color conatians this number of character $c its not start with a or b");
+    }
+  }
+  ;
+}
